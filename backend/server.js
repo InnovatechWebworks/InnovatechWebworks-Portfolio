e5d3db9eeedfd30818b6c24f74fe95b58f80938a
Innovatech WebWorks <innovatechwebworks@gmail.com>
2025-07-03 15:41:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,  // add this in .env
+}));
+
 app.use(express.json());
 
 // API Routes
